81246ccb5c63de0f912c5cf2c14b5b4385097893
Law Zer Kye, Keith <keith.law.2021@scis.smu.edu.sg>
2022-10-26 14:01:42 +0800

    Revert "pushy push for loginview"
    This reverts commit b09ea7c0f264b5e78a20d620e23be990cb7cfa9d.

--- wyd/src/views/LoginView.vue
+++ wyd/src/views/LoginView.vue
@@ -1,31 +1,9 @@
 <template>
-     <div class="context">
-        <h1>Wad Are You 2 Doing</h1>
-      <div>
-        <button v-on:click="googleSignIn" 
-        class="glow-on-hover" type="button">
-          Sign In with Google
-        </button>
-      </div>
-    </div>
-
-
-<div class="area" >
-            <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    </div >
-
-
+  <div>
+    <button v-on:click="googleSignIn">
+      Sign In with Google
+    </button>
+  </div>
 </template>
 <script>
 import Navbar from '../components/layouts/navbar.vue'
@@ -84,11 +62,4 @@ export default {
     color: #42b983;
   }
   </style>
-
-  <style>
-    @import '../LoginView.css';
-  </style>
-
-
-
   
