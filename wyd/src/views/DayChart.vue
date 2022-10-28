dd97e7e89a6c545c3290996438eae0a5cc58ce0a
minnalk <94851324+minnalk@users.noreply.github.com>
2022-10-28 22:17:14 +0800

    chart appeared... changed spending dashboard to a grid

--- wyd/src/views/DayChart.vue
+++ wyd/src/views/DayChart.vue
@@ -56,18 +56,37 @@
             chartData: {
                 type: Object,
                 default: () => ({
-                    labels: [],
-                    datasets: []
-                })
-            },
-            chartOptions: {
-                type: Object,
-                default: () => ({
-                    labels: [],
-                    datasets: []
-                })
+                    // labels: ["numbers"],
+                    // datasets: [[12, 19, 3, 5, 2, 3]]
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ]
+                            }]
+            }),
+            // chartOptions: {
+            //     type: Object,
+            //     default: () => ({
+            //         labels: [],
+            //         datasets: [12, 19, 3, 5, 2, 3]
+            //     })
             }
         },
+//   computed: {
+//     myStyles () {
+//       return {
+//         height: '50%',
+//         position: 'relative'
+//       }
+//     },
     
         setup(props) {
             return () =>
