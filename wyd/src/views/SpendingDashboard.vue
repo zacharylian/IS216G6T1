517fd22a048e8056d3df1ff7813a46603ceb11b3
minnalk <94851324+minnalk@users.noreply.github.com>
2022-10-31 21:43:53 +0800

    Revert "Updated from previous push"
    This reverts commit 934a3534066b6851c79041518bdb461b1c77caf0.

--- wyd/src/views/SpendingDashboard.vue
+++ wyd/src/views/SpendingDashboard.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <navigation-bar></navigation-bar>
+<<<<<<< HEAD
+    <div class="border border-info" style="height: 95%">
+      <div class="row border border-primary h-50">
+        <div class="col border border-danger w-50">
+          <DayChart></DayChart>
+=======
     <div class="border border-info" style="height:95%">
         <div class="row border border-primary h-50">
             <div class="col border border-danger w-50">
@@ -14,6 +20,7 @@
             <div class="col border border-primary">
                 zach put ur d3 here
             </div>
+>>>>>>> parent of 6f2cf9b (Revert "idky not clickable but yay...")
         </div>
         <div class="col border border-success w-50">today</div>
       </div>
@@ -22,6 +29,7 @@
         <CalendarHeatmap />
       </div>
     </div>
+  </div>
 </template>
 
 <script>
