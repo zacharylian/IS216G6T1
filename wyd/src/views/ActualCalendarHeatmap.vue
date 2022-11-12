04b35921d7e25e1f878df6c8b44a28982712a866
Zachary Lian <zachary.lian@gmail.com>
2022-11-13 02:31:45 +0800

    i need help pls

--- wyd/src/views/ActualCalendarHeatmap.vue
+++ wyd/src/views/ActualCalendarHeatmap.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="d3sucks">
-    <svg class="chart"></svg>
-  </div>
+  <svg class="chart"></svg>
 </template>
 
 <script>
-import CalendarJs from  "../plugins/calendar";
+import CalendarJs from  "../plugins/calendar"
 
 export default {
   name: "CalendarHeatmap",
   mounted() {
     const d3Script = document.createElement("script");
-    d3Script.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js");
+    d3Script.setAttribute("src", "https://d3js.org/d3.v7.min.js");
+    d3Script.setAttribute("charset", "utf-8")
     document.head.appendChild(d3Script);
 
-    const labratScript = document.createElement("script")
-    labratScript.setAttribute("src", "http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js")
-    document.head.appendChild(labratScript);
+    // const d3Script = document.createElement("script");
+    // d3Script.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js");
+    // d3Script.setAttribute("charset", "utf-8")
+    // document.head.appendChild(d3Script);
+
+    // const labratScript = document.createElement("script")
+    // labratScript.setAttribute("src", "http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js")
+    // document.head.appendChild(labratScript);
+
+    // const d3fix = document.createElement("script");
+    // d3fix.setAttribute("src", "http://d3js.org/d3.v3.min.js");
+    // d3fix.setAttribute("charset", "utf-8")
+    // document.head.appendChild(d3fix);
 
     CalendarJs()
   },
