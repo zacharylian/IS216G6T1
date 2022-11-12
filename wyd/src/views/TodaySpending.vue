5b93b0ed7fad52feed5b3f92045eaa1b75414797
James Teo <teojames.jh@gmail.com>
2022-11-12 23:42:49 +0800

    meowmeow spendings

--- wyd/src/views/TodaySpending.vue
+++ wyd/src/views/TodaySpending.vue
@@ -39,10 +39,24 @@
 </template>
 
 <script>
+import {addDocs, collection, getDoc, doc, firestoreAction, setDoc, updateDoc} from 'firebase/firestore';
+import { db } from '../main';
+import { getAuth, signOut } from '@firebase/auth';
+// import {getDate}
 
     export default {
+        created(){
+            console.log("=====getting UID=====")
+            this.uid = getAuth().currentUser.uid
+            console.log(this.uid)
+
+            console.log("=====extracting data from db=====")
+            this.checkdb()
+        },
         data() {
             return {
+                date: getDate(new Date),
+                uid: "",
                 last_spending: 0,
                 total: 100,
                 category: 'Food',
@@ -58,9 +72,32 @@
         },
         
         methods : {
+            async checkdb(){
+                const docRef = doc(db, "spendings", this.uid);
+                const docSnap = await getDoc(docRef);
+
+                if (docSnap.exists()) {
+                    console.log("Document data:", docSnap.data())
+                    this.last_spending = docSnap.data().lastSpending
+                    this.total = docSnap.data().total/30 - docSnap.data().daily[this.date].amt
+
+
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                    setDoc(docRef, {total: 1000, lastSpending: 0, daily: {1:{amt: 0},2:{amt: 0},3:{amt: 0},4:{amt: 0},5:{amt: 0},6:{amt: 0},7:{amt: 0},8:{amt: 0},9:{amt: 0},10:{amt: 0},11:{amt: 0},12:{amt: 0},13:{amt: 0},14:{amt: 0},15:{amt: 0},16:{amt: 0},17:{amt: 0},18:{amt: 0},19:{amt: 0},20:{amt: 0},21:{amt: 0},22:{amt: 0},23:{amt: 0},24:{amt: 0},25:{amt: 0},26:{amt: 0},27:{amt: 0},28:{amt: 0},29:{amt: 0},30:{amt: 0},31:{amt: 0}} } );
+                }
+            },
+
+            async updatedb(){
+                const docRef = doc(db, "spendings", this.uid);
+                await updateDoc(docRef, { total: this.total, lastSpending: this.last_spending })
+            },
+
             deduct() {
                 this.total -= this.last_spending
                 console.log(this.total)
+                this.updatedb()
                 return this.total
             }
         }
